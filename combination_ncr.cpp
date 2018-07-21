cee84943582d9a896847885e5c86121bd65542d7
Muhamed Salih Seyed Ibrahim <30931525+MuhamedSalihSeyedIbrahim@users.noreply.github.com>
2018-07-21 21:02:04 +0530

    Create combination_ncr.cpp

---
+++ combination_ncr.cpp
@@ -0,0 +1,13 @@
+long long combination(int n, int r) {
+    if(r > n / 2) r = n - r; 
+    // because C(n, r) == C(n, n - r)
+    long long ans = 1;
+    int i;
+
+    for(i = 1; i <= r; i++) {
+        ans *= n - r + i;
+        ans /= i;
+    }
+
+    return ans;
+}
